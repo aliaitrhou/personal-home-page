67701de19b68e9f38ae3852a0c5157103c21c54d
aliaitrahou <alirhou7@gmail.com>
2025-01-31 13:37:24 +0100

    fix(certificates): complete the page functionality and improve the ui

--- app/certificates/page.tsx
+++ app/certificates/page.tsx
@@ -1,16 +1,53 @@
+"use client";
+
 import React from "react";
-import webDesign from "@/public/certificates/web-design.png";
+import { useSearchParams } from "next/navigation";
+import fcc from "@/public/certificates/web-design.png";
 import infomath from "@/public/certificates/infomath.png";
 import ai from "@/public/certificates/ai-path.png";
+import advancedReact from "@/public/certificates/react.png";
 import CertificateBox from "@/components/certificate-box";
 
+const certs = [
+  {
+    src: fcc,
+    name: "web-design",
+    alt: "web design certeficate",
+  },
+  {
+    src: infomath,
+    name: "infomath",
+    alt: "design officer certeficate",
+  },
+  {
+    src: ai,
+    name: "ai-developer",
+    alt: "ai dev certeficate",
+  },
+  {
+    src: advancedReact,
+    name: "advnaced-react",
+    alt: "advanced react certeficate",
+  },
+];
+
 const Certificates = () => {
+  const params = useSearchParams();
+
+  const certToOpen = params.get("cert");
+
   return (
-    <div className="w-full px-4 sm:px-6 md:px-10 mx-auto pt-4">
-      <section className="grid grid-cols-1 md:grid-cols-2 md:gap-8 space-y-8 sm:space-y-10 md:space-y-12 px-0 sm:px-4 md:px-8">
-        <CertificateBox alt="web design certificate" path={infomath} />
-        <CertificateBox alt="ai dev certificate" path={ai} />
-        <CertificateBox alt="design officer certificate" path={webDesign} />
+    <div className="w-full px-4 sm:px-6  mx-auto pt-4">
+      <section className="flex flex-col gap-8 sm:gap-4">
+        {certs.map((cert, index) => (
+          <CertificateBox
+            key={index}
+            position={index % 2 == 0 ? "sm:justify-start" : "sm:justify-end"}
+            alt={cert.alt}
+            path={cert.src}
+            openCert={cert.name === certToOpen ? true : false}
+          />
+        ))}
       </section>
     </div>
   );
--- components/certificate-box.tsx
+++ components/certificate-box.tsx
@@ -1,18 +1,64 @@
-import React from "react";
-import Image, { StaticImageData } from "next/image";
+"use client";
 
-interface certificateProps {
+import React, { useEffect, useState } from "react";
+import Image, { StaticImageData } from "next/image";
+import { useSearchParams } from "next/navigation";
+
+interface CertificateProps {
   alt: string;
   path: StaticImageData;
+  position: string;
+  openCert: boolean;
 }
 
-const CertificateBox: React.FC<certificateProps> = ({ alt, path }) => {
+const CertificateBox: React.FC<CertificateProps> = ({
+  alt,
+  path,
+  position,
+  openCert = false,
+}) => {
+  const [expand, setExpand] = useState(openCert || false);
+  const searchParams = useSearchParams();
+
+  // i used this function to remove the cert query param from the url
+  // cause it keeps the infomath certification open
+  const removeQueryParams = () => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.forEach((_, key) => {
+      params.delete(key);
+    });
+    const newUrl = `${window.location.pathname}?${params.toString()}`;
+    window.history.replaceState(null, "", newUrl);
+  };
   return (
-    <Image
-      src={path}
-      alt={alt}
-      className="object-fill aspect-[14/8] rounded-lg border border-zinc-500 dark:border-zinc-300 shadow-xl"
-    />
+    <>
+      <div className={`flex justify-center ${position}`}>
+        <div className="relative max-w-full sm:max-w-[55%] rounded-lg shadow-md dark:shadow-black overflow-hidden">
+          <Image
+            src={path}
+            alt={alt}
+            className="object-fill w-full aspect-[14/8] inner-custom rounded-lg sm:cursor-pointer border-2 hover:border-orange-500 dark:hover:border-NeonLime-600"
+            onClick={() => {
+              setExpand(true);
+              removeQueryParams();
+            }}
+          />
+        </div>
+      </div>
+
+      {expand && (
+        <div
+          className="hidden fixed inset-0 bg-black bg-opacity-80 sm:flex items-center justify-center z-50"
+          onClick={() => setExpand(false)}
+        >
+          <Image
+            src={path}
+            alt={alt}
+            className="max-w-[80%] max-h-[85%] md:max-w-[60%] md:max-h-[65%] rounded-xl"
+          />
+        </div>
+      )}
+    </>
   );
 };
 
