9f0426748bf1cc8b28d30b421bea1c7ce0b26003
aliaitrahou <alirhou7@gmail.com>
2025-02-01 23:37:32 +0100

    fix(toggle-theme): remove initial SSR

--- components/toggle-theme.tsx
+++ components/toggle-theme.tsx
@@ -5,6 +5,7 @@ import { FiSun } from "react-icons/fi";
 import { FiMoon } from "react-icons/fi";
 
 const ToggleTheme = () => {
+  const [mounted, setMounted] = useState(false);
   const [t, setT] = useState(() => {
     // in the first render this condition won't accour (currentTheme) doesn't exist yet
     if (
@@ -15,7 +16,10 @@ const ToggleTheme = () => {
     }
 
     // checking for system current theme
-    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    if (
+      typeof window != "undefined" &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
       return "dark";
     }
 
@@ -28,8 +32,9 @@ const ToggleTheme = () => {
     setT(currentTheme);
   }
 
-  // changing the 't' well triger reRun this useEffect
+  // changing the 't' well triger this useEffect
   useEffect(() => {
+    setMounted(true);
     if (t === "light") {
       document.documentElement.classList.remove("dark");
     } else {
@@ -37,18 +42,24 @@ const ToggleTheme = () => {
     }
   }, [t]);
 
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   return (
-    <button
-      className="group z-10 p-[9px] rounded-full bg-orange-500 hover:bg-orange-600 group-hover:bg-orange-600 dark:bg-NeonLime-500 dark:hover:bg-NeonLime-600 focus:outline-none"
-      onClick={toggleLocalStorageValue}
-    >
-      {t === "light" ? (
-        <FiMoon className="text-white" />
-      ) : (
-        <FiSun color="black" />
-      )}
-      <div className="absolute -z-10 top-0 right-[23.5px] hidden sm:inline-block w-1 h-[50%]  bg-orange-500 group-hover:bg-orange-600 dark:bg-NeonLime-500 dark:group-hover:bg-NeonLime-600" />
-    </button>
+    mounted && (
+      <button
+        className="group z-10 p-[9px] rounded-full bg-orange-500 hover:bg-orange-600 group-hover:bg-orange-600 dark:bg-NeonLime-500 dark:hover:bg-NeonLime-600 focus:outline-none"
+        onClick={toggleLocalStorageValue}
+      >
+        {t === "light" ? (
+          <FiMoon className="text-white" />
+        ) : (
+          <FiSun color="black" />
+        )}
+        <div className="absolute -z-10 top-0 right-[23.5px] hidden sm:inline-block w-1 h-[50%]  bg-orange-500 group-hover:bg-orange-600 dark:bg-NeonLime-500 dark:group-hover:bg-NeonLime-600" />
+      </button>
+    )
   );
 };
 
