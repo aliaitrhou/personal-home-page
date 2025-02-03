f10a81aa9267053f693d2dd915da79d2fd65c741
aliaitrahou <alirhou7@gmail.com>
2025-02-03 17:54:51 +0100

    feat(toggle-theme): animate when theme toggle

--- components/toggle-theme.tsx
+++ components/toggle-theme.tsx
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from "react";
 import { FiSun } from "react-icons/fi";
 import { FiMoon } from "react-icons/fi";
+import { AnimatePresence, motion } from "framer-motion";
 
 const ToggleTheme = () => {
   const [mounted, setMounted] = useState(false);
@@ -48,17 +49,26 @@ const ToggleTheme = () => {
 
   return (
     mounted && (
-      <button
-        className="group z-10 p-[9px] rounded-full bg-orange-500 hover:bg-orange-600 group-hover:bg-orange-600 dark:bg-NeonLime-500 dark:hover:bg-NeonLime-600 focus:outline-none"
-        onClick={toggleLocalStorageValue}
-      >
-        {t === "light" ? (
-          <FiMoon className="text-white" />
-        ) : (
-          <FiSun color="black" />
-        )}
-        <div className="absolute -z-10 top-0 right-[23.5px] hidden sm:inline-block w-1 h-[50%]  bg-orange-500 group-hover:bg-orange-600 dark:bg-NeonLime-500 dark:group-hover:bg-NeonLime-600" />
-      </button>
+      <AnimatePresence mode="wait" initial={false}>
+        <button
+          className="group z-10 p-[9px] w-fit rounded-full bg-orange-500 hover:bg-orange-600 group-hover:bg-orange-600 dark:bg-NeonLime-500 dark:hover:bg-NeonLime-600 focus:outline-none"
+          onClick={toggleLocalStorageValue}
+        >
+          <motion.div
+            key={t}
+            initial={{ x: 8, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.4 }}
+          >
+            {t === "light" ? (
+              <FiMoon className="text-white" />
+            ) : (
+              <FiSun color="black" />
+            )}
+          </motion.div>
+          <div className="absolute -z-10 top-0 right-[23.5px] hidden sm:inline-block w-1 h-[50%]  bg-orange-500 group-hover:bg-orange-600 dark:bg-NeonLime-500 dark:group-hover:bg-NeonLime-600" />
+        </button>
+      </AnimatePresence>
     )
   );
 };
