c29375f50e964714405d2ad56ae3f645bec1a752
aliaitrahou <alirhou7@gmail.com>
2025-02-13 20:50:32 +0100

    fix(title): responsive animation layout

--- components/page-title.tsx
+++ components/page-title.tsx
@@ -10,23 +10,21 @@ interface Props {
 
 const PageTitle: React.FC<Props> = ({ title, keyword }) => {
   return (
-    <div className="relative flex flex-col ml-[5%] sm:ml-[20%] justify-center items-center text-center sm:w-[55%]">
+    <div className="relative flex flex-col -ml-[5%] sm:ml-[20%] justify-center items-center text-center w-full sm:w-[55%]">
       <div className="uppercase text-sm sm:text-md mb-1">{keyword}</div>
 
-      <div className="relative h-12 sm:h-16 w-full overflow-hidden">
-        <AnimatePresence mode="wait">
-          <motion.div
-            key={title}
-            initial={{ rotateX: 90, opacity: 0, y: 20 }}
-            animate={{ rotateX: 0, opacity: 1, y: 0 }}
-            exit={{ rotateX: -90, opacity: 0, y: -20 }}
-            transition={{ duration: 0.5, ease: "easeInOut", delay: 0.5 }}
-            className="absolute w-full text-2xl xs:text-3xl sm:text-4xl font-mplus font-medium"
-          >
-            {title}
-          </motion.div>
-        </AnimatePresence>
-      </div>
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={title}
+          initial={{ scale: 0.7, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          exit={{ scale: 0.7, opacity: 0 }}
+          transition={{ duration: 0.5, ease: "easeInOut", delay: 0.6 }}
+          className="w-full text-2xl xs:text-3xl sm:text-4xl font-mplus font-medium text-center"
+        >
+          {title}
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 };
