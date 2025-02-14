1a26c2ee95764820ae92af953f88659fc0661b60
aliaitrahou <alirhou7@gmail.com>
2025-02-14 15:37:04 +0100

    style(footer): increase margin

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-full text-center my-4">
+    <footer className="w-full text-center my-10">
       <p className="text-zinc-700/40 dark:text-zinc-600 text-sm sm:text-md">
         © {new Date().getFullYear()} Ali Ait Rahou. All Rights Reserved.
       </p>
