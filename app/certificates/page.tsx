9565aeb63957c25f646d1bb46603fb152f9822b8
aliaitrahou <alirhou7@gmail.com>
2025-02-15 16:09:07 +0100

    refactor(certificate): change the certificates layout

--- app/certificates/page.tsx
+++ app/certificates/page.tsx
@@ -11,8 +11,8 @@ const Certificates = () => {
   const certToOpen = params.get("cert");
 
   return (
-    <div className="w-full px-4 sm:px-6  mx-auto pt-4 pb-8">
-      <section className="grid grid-cols-1 sm:grid-cols-2 gap-8 sm:gap-10">
+    <div className="w-full px-4 sm:px-6  mx-auto py-12">
+      <section className="grid grid-cols-1 sm:grid-cols-2 gap-8 sm:gap-x-10 md:gap-x-24 sm:gap-y-6 md:gap-y-16 px-0 xs:px-2 sm:px-6">
         {certs.map((cert, index) => (
           <CertificateBox
             key={index}
