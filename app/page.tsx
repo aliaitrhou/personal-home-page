95dabec5cb05548de5c233ebe3a08e46ace231d5
aliaitrahou <alirhou7@gmail.com>
2025-02-15 16:10:04 +0100

    style(pages): improve styling

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 
 const Home = () => {
   return (
-    <div className="w-full px-3 sm:px-6 md:px-10 mx-auto py-4 space-y-3 xs:space-y-4 sm:space-y-8">
+    <div className="w-full px-3 sm:px-6 md:px-10 mx-auto py-4 space-y-6 sm:space-y-8">
       <SectionWrapper delay={0}>
         <ContentBox title="Who is Ali">
           <p className="text-justify indent-8 font-light text-lg xs:text-xl text-zinc-700 dark:text-zinc-300">
@@ -16,11 +16,17 @@ const Home = () => {
             student and aspiring software developer based in Morocco. Driven by
             the desire to create stuff that meets my needs and ambitions, I love
             everything related to product creation from identifying problems to
-            designing and building software that not only makes me happy but
-            also motivates me to push forward and tackle the next challenge.
-            I’ve spent countless hours refining my craft, optimizing workflows,
-            and pushing my limits. And i plan to code until AI rips software
-            from my speedy Vim fingers.
+            designing and building&nbsp;
+            <Link
+              href={"/projects"}
+              className="text-blue-500 dark:text-teal-500 hover:underline underline-offset-2 decoration-blue-500 dark:decoration-teal-500"
+            >
+              Projects
+            </Link>{" "}
+            that not only makes me happy but also motivates me to push forward
+            and tackle the next challenge. I’ve spent countless hours refining
+            my craft, optimizing workflows, and pushing my limits. And i plan to
+            code until AI rips software from my speedy Vim fingers.
           </p>
         </ContentBox>
       </SectionWrapper>
