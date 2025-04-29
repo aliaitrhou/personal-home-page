cce26207ef92a105d92c1ee8c5ca10cad822d543
aliaitrahou <alirhou7@gmail.com>
2025-04-29 14:10:25 +0100

    feat(nav): add opened project page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,8 +5,9 @@ import Logo from "./logo";
 import LinkBox from "./linkbox";
 import ToggleTheme from "./toggle-theme";
 import MobileMenu from "./mobileMenu";
+import { IoIosArrowRoundForward } from "react-icons/io";
 import { usePathname } from "next/navigation";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 interface NavProps {
   url: string;
@@ -21,6 +22,8 @@ const Navbar: React.FC<NavProps> = ({ url, classNames }) => {
     : null;
   const isActiveProjectDetail = /^\/projects\/[^/]+$/.test(pathname);
 
+  const showDetailPageSlug = projectSlug && isActiveProjectDetail;
+
   return (
     <nav
       className={`fixed top-0 left-0 right-0 z-40 w-full bg-[#ffffff40] dark:bg-[#20202380] backdrop-blur-sm ${classNames}`}
@@ -32,32 +35,46 @@ const Navbar: React.FC<NavProps> = ({ url, classNames }) => {
           </h1>
         </div>
         <ul className="hidden flex-grow sm:flex flex-col sm:flex-row w-full sm:w-auto items-center mt-4 sm:mt-0">
-          <LinkBox url={url} href="/projects" classNames="pr-1">
+          <LinkBox
+            url={url}
+            href="/projects"
+            classNames={showDetailPageSlug ? "py-2 pr-[2px]" : " "}
+          >
             Projects
           </LinkBox>
-          {isActiveProjectDetail && projectSlug && (
-            <>
-              <motion.div className="inline text-xs">&gt;</motion.div>
+          <AnimatePresence mode="wait">
+            {showDetailPageSlug && (
               <motion.div
-                initial={{
-                  opacity: 0,
-                  width: 0,
+                className="flex items-center"
+                initial="hidden"
+                animate="visible"
+                exit="hidden"
+                variants={{
+                  visible: { transition: { staggerChildren: 0.1 } }, // slight delay between icon & slug
                 }}
-                animate={{
-                  opacity: 1,
-                  width: 55,
-                }}
-                exit={{
-                  opacity: 0,
-                  width: 0,
-                }}
-                transition={{ duration: 1 }}
-                className={`ml-1 py-2 text-sm mr-1 font-light hover:underline hover:decoration-1 hover:underline-offset-4 text-orange-500 dark:text-NeonLime-500`}
+                key={projectSlug}
               >
-                {decodeURIComponent(projectSlug)}
+                <motion.div
+                  initial={{ opacity: 0, x: -10 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  exit={{ opacity: 0, x: -10 }}
+                  transition={{ duration: 0.3 }}
+                >
+                  <IoIosArrowRoundForward />
+                </motion.div>
+
+                <motion.div
+                  initial={{ opacity: 0, x: -10 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  exit={{ opacity: 0, x: -10 }}
+                  transition={{ duration: 0.3, delay: 0.2 }} // delay slug after icon
+                  className="whitespace-nowrap break-keep ml-1 py-2 text-xs mr-1 font-light text-orange-500 dark:text-NeonLime-500"
+                >
+                  {decodeURIComponent(projectSlug)}
+                </motion.div>
               </motion.div>
-            </>
-          )}
+            )}
+          </AnimatePresence>
           <LinkBox url={url} href="/certificates">
             Certificates
           </LinkBox>
