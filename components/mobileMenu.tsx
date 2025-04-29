b7d4a2991204c5648d16d4ca93a4a5f5c72a1777
aliaitrahou <alirhou7@gmail.com>
2025-04-29 16:29:32 +0100

    feat(mobile-menu): current opened project page

--- components/mobileMenu.tsx
+++ components/mobileMenu.tsx
@@ -4,10 +4,21 @@ import React, { useState, useEffect, useRef } from "react";
 import { IoMenu } from "react-icons/io5";
 import LinkBox from "./linkbox";
 import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
+import { BsArrowReturnRight } from "react-icons/bs";
 
 const MobileMenu = ({ path }: { path: string }) => {
   const [openMenu, setOpenMenu] = useState(false);
   const menuRef = useRef<null | HTMLDivElement>(null);
+
+  const pathname = usePathname();
+
+  const projectSlug = pathname.startsWith("/projects/")
+    ? pathname.split("/")[2]
+    : null;
+  const isActiveProjectDetail = /^\/projects\/[^/]+$/.test(pathname);
+
+  const showDetailPageSlug = projectSlug && isActiveProjectDetail;
 
   // clicking outside of the menu should close it
   useEffect(() => {
@@ -81,6 +92,39 @@ const MobileMenu = ({ path }: { path: string }) => {
               >
                 Projects
               </LinkBox>
+              <AnimatePresence mode="wait">
+                {showDetailPageSlug && (
+                  <motion.div
+                    className="flex items-center ml-4"
+                    initial="hidden"
+                    animate="visible"
+                    exit="hidden"
+                    variants={{
+                      visible: { transition: { staggerChildren: 0.1 } }, // slight delay between icon & slug
+                    }}
+                    key={projectSlug}
+                  >
+                    <motion.div
+                      initial={{ opacity: 0, x: -10 }}
+                      animate={{ opacity: 1, x: 0 }}
+                      exit={{ opacity: 0, x: -10 }}
+                      transition={{ duration: 0.3 }}
+                    >
+                      <BsArrowReturnRight className="size-3" />
+                    </motion.div>
+
+                    <motion.div
+                      initial={{ opacity: 0, x: -10 }}
+                      animate={{ opacity: 1, x: 0 }}
+                      exit={{ opacity: 0, x: -10 }}
+                      transition={{ duration: 0.3, delay: 0.2 }} // delay slug after icon
+                      className="whitespace-nowrap break-keep ml-1 text-xs mr-1 font-light text-orange-500 dark:text-NeonLime-500"
+                    >
+                      {decodeURIComponent(projectSlug)}
+                    </motion.div>
+                  </motion.div>
+                )}
+              </AnimatePresence>
               <LinkBox
                 url={path}
                 href="/certificates"
