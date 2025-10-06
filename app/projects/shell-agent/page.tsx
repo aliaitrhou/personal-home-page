b7d45c8107bc95f59ef65392a594a39c1e06f729
aliaitrahou <alirhou7@gmail.com>
2025-10-06 17:55:36 +0100

    docs(projects): update shellagent project with new assets

--- app/projects/shell-agent/page.tsx
+++ app/projects/shell-agent/page.tsx
@@ -5,12 +5,12 @@ import ProjectMetaData, { ToolBox } from "@/components/project-meta-data";
 import AnimationBox from "@/components/animation-box";
 import Image from "next/image";
 import terminal from "@/public/imgs/projects/shell_agent/shell_agent_terminal.png";
-// import shell_agent_v1 from "@/public/imgs/projects/shell_agent/shell_agent_v1.png";
-import shell_agent_2 from "@/public/imgs/projects/shell_agent/shell_agent_2.png";
-import shell_agent_3 from "@/public/imgs/projects/shell_agent/shell_agent_3.png";
-import { FaNodeJs } from "react-icons/fa";
+import overview from "@/public/imgs/projects/shell_agent/overview.png";
+import user_guide from "@/public/imgs/projects/shell_agent/user-guide.png";
+import { FaDocker, FaNodeJs } from "react-icons/fa";
 import { RiDatabase2Fill, RiNextjsFill } from "react-icons/ri";
 import { SiClerk, SiPrisma } from "react-icons/si";
+import { DiRedis } from "react-icons/di";
 
 const ShellAgent = () => {
   return (
@@ -23,7 +23,9 @@ const ShellAgent = () => {
             memorizing it, I thought—why not turn it into an assistant that
             explains itself? That question led to this AI-powered tool.
           </p>
-          <Image alt="shell agent terminal" src={terminal} />
+          <Image alt="shell agent terminal"
+            className="my-2 rounded-lg border-4  border-zinc-700 dark:border-zinc-900"
+            src={terminal} />
           <p className="text-justify">
             Over time, I expanded it so users can now learn and run terminal
             commands directly in the browser, and even interact with real course
@@ -63,7 +65,21 @@ const ShellAgent = () => {
               PrismaORM,
             </ToolBox>
             <ToolBox
+              color="blue"
+              icon={<FaDocker className="size-4" />}
+              href="https://docker.com"
+            >
+              Docker,
+            </ToolBox>
+            <ToolBox
               color="red"
+              icon={<DiRedis className="size-4" />}
+              href="https://redis.io"
+            >
+              Redis,
+            </ToolBox>
+            <ToolBox
+              color="orange"
               icon={<RiDatabase2Fill className="size-3" />}
               href="https://neon.tech"
             >
@@ -71,7 +87,7 @@ const ShellAgent = () => {
             </ToolBox>
           </ProjectMetaData>
           <ProjectMetaData title="Hosting">
-            Deployed to internet using Vercel.
+            Deployed to internet using Digital Ocean.
           </ProjectMetaData>
           <ProjectMetaData title="Website">
             <Link
@@ -93,14 +109,19 @@ const ShellAgent = () => {
           </ProjectMetaData>
           <Image
             alt="shell agent landing"
-            src={shell_agent_2}
+            src={overview}
             className="my-8 rounded-md"
           />
-          <Image
-            alt="shell agent workspace"
-            src={shell_agent_3}
-            className="mb-8 rounded-md"
-          />
+          <p className="mb-2">User Guide :</p>
+
+          <Link href="https://shellagent.ai/user-guide" target="_blank"
+          >
+            <Image
+              alt="shell agent workspace"
+              src={user_guide}
+              className="mb-8 rounded-md"
+            />
+          </Link>
         </ContentBox>
       </AnimationBox>
     </div>
