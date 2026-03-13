edd68d4592c15dcb769cb7343bfd6398f10817b3
aliaitrahou <alirhou7@gmail.com>
2026-03-13 23:57:36 +0000

    refactor(voxel-office): change voxel office model dimentions

--- components/voxel-office.tsx
+++ components/voxel-office.tsx
@@ -1,115 +1,113 @@
-import { useState, useEffect, useRef, useCallback } from 'react'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import { loadGLTFModel } from '../lib/model'
-import { OfficeSpinner, OfficeContainer } from './voxel-office-loader'
-
+import { useState, useEffect, useRef, useCallback } from "react";
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { loadGLTFModel } from "../lib/model";
+import { OfficeSpinner, OfficeContainer } from "./voxel-office-loader";
 
 const VoxelOffice: React.FC = () => {
-  const refContainer = useRef<HTMLDivElement>(null)
-  const [loading, setLoading] = useState<boolean>(true)
-  const refRenderer = useRef<THREE.WebGLRenderer>(null)
-  
-  const urlDogGLB = "/3d_obj/office.glb"
-  
+  const refContainer = useRef<HTMLDivElement>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const refRenderer = useRef<THREE.WebGLRenderer>(null);
+
+  const urlDogGLB = "/3d_obj/office.glb";
+
   const handleWindowResize = useCallback(() => {
-    const { current: renderer } = refRenderer
-    const { current: container } = refContainer
+    const { current: renderer } = refRenderer;
+    const { current: container } = refContainer;
     if (container && renderer) {
-      const scW = container.clientWidth
-      const scH = container.clientHeight
-      renderer.setSize(scW, scH)
+      const scW = container.clientWidth;
+      const scH = container.clientHeight;
+      renderer.setSize(scW, scH);
     }
-  }, [])
-   
+  }, []);
+
   useEffect(() => {
-    const { current: container } = refContainer
+    const { current: container } = refContainer;
     if (container) {
-      const scW = container.clientWidth
-      const scH = container.clientHeight
-      
+      const scW = container.clientWidth;
+      const scH = container.clientHeight;
+
       const renderer = new THREE.WebGLRenderer({
         antialias: true,
-        alpha: true
-      })
-      renderer.setPixelRatio(window.devicePixelRatio)
-      renderer.setSize(scW, scH)
-      renderer.outputColorSpace = THREE.SRGBColorSpace
-      container.appendChild(renderer.domElement)
-      refRenderer.current = renderer
-      
-      const scene = new THREE.Scene()
-      
-      const target = new THREE.Vector3(-0.5, 0.3, 0)
+        alpha: true,
+      });
+      renderer.setPixelRatio(window.devicePixelRatio);
+      renderer.setSize(scW, scH);
+      renderer.outputColorSpace = THREE.SRGBColorSpace;
+      container.appendChild(renderer.domElement);
+      refRenderer.current = renderer;
+
+      const scene = new THREE.Scene();
+
+      const target = new THREE.Vector3(-0.5, 0.3, 0);
       const initialCameraPosition = new THREE.Vector3(
         20 * Math.sin(0.2 * Math.PI),
         10,
-        20 * Math.cos(0.2 * Math.PI)
-      )
+        20 * Math.cos(0.2 * Math.PI),
+      );
 
-      const scale = scH * 0.0025 + 1.2
+      const scale = scH * 0.0062 + 1.2;
       const camera = new THREE.OrthographicCamera(
         -scale,
         scale,
         scale,
         -scale,
         0.01,
-        50000
-      )
-      camera.position.copy(initialCameraPosition)
-      camera.lookAt(target)
-      
-      const ambientLight = new THREE.AmbientLight(0xcccccc, Math.PI)
-      scene.add(ambientLight)
-      
-      const controls = new OrbitControls(camera, renderer.domElement)
-      controls.autoRotate = true
-      controls.autoRotateSpeed = 2.0  // Adjust speed of rotation
-      controls.target = target
-      controls.enableDamping = true
-      controls.dampingFactor = 0.05
-      
-      let req: number | null = null
-      
+        50000,
+      );
+      camera.position.copy(initialCameraPosition);
+      camera.lookAt(target);
+
+      const ambientLight = new THREE.AmbientLight(0xcccccc, Math.PI);
+      scene.add(ambientLight);
+
+      const controls = new OrbitControls(camera, renderer.domElement);
+      controls.autoRotate = true;
+      controls.autoRotateSpeed = 2.0; // Adjust speed of rotation
+      controls.target = target;
+      controls.enableDamping = true;
+      controls.dampingFactor = 0.05;
+
+      let req: number | null = null;
+
       const animate = () => {
-        req = requestAnimationFrame(animate)
-        
-        controls.update()
-        renderer.render(scene, camera)
-      }
-      
+        req = requestAnimationFrame(animate);
+
+        controls.update();
+        renderer.render(scene, camera);
+      };
+
       loadGLTFModel(scene, urlDogGLB, {
         receiveShadow: false,
-        castShadow: false
+        castShadow: false,
       }).then(() => {
-        animate()
-        setLoading(false)
-      })
-      
+        animate();
+        setLoading(false);
+      });
+
       return () => {
         if (req !== null) {
-          cancelAnimationFrame(req)
+          cancelAnimationFrame(req);
         }
-        controls.dispose()
-        renderer.domElement.remove()
-        renderer.dispose()
-      }
+        controls.dispose();
+        renderer.domElement.remove();
+        renderer.dispose();
+      };
     }
-  }, [])
-  
+  }, []);
+
   useEffect(() => {
-    window.addEventListener('resize', handleWindowResize, false)
+    window.addEventListener("resize", handleWindowResize, false);
     return () => {
-      window.removeEventListener('resize', handleWindowResize, false)
-    }
-  }, [handleWindowResize])
-  
+      window.removeEventListener("resize", handleWindowResize, false);
+    };
+  }, [handleWindowResize]);
+
   return (
     <OfficeContainer ref={refContainer}>
       {loading && <OfficeSpinner />}
     </OfficeContainer>
-  )
-}
+  );
+};
 
-export default VoxelOffice
-
+export default VoxelOffice;
