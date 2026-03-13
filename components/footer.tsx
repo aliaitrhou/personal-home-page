85b20c4027ad8fdf99856768c249313fee960a97
aliaitrahou <alirhou7@gmail.com>
2026-03-13 23:57:01 +0000

    style(footer): change font

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { lexend } from "@/app/fonts";
 
 const Footer = () => {
   return (
-    <footer className="w-full text-center my-10">
+    <footer
+      className={`w-full text-center my-10 ${lexend.className} font-light`}
+    >
       <p className="text-zinc-700/40 dark:text-zinc-600 text-sm sm:text-md">
         © {new Date().getFullYear()} Ali Ait Rahou. All Rights Reserved.
       </p>
